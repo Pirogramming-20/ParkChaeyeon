1cd07a64bd47bdd7e4506c0f6e34a0bcd26476ed
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2024-01-04 22:09:19 +0900

    8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,14 +1,11 @@
-#A
-num=0
-BR31 = [i for i in range(1, 32)]
-누적num=0
 
-while(누적num<31):
-    while True:
+def brGame():
+     while True:
         try:
             num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
             if 1 <= num <= 3:
-                break
+                return num
+                
             else:
                 print("1, 2, 3 중 하나를 입력하세요.")
         #입력받은 값을 int()를 사용하여 정수로 변환을 시도 but 실패시 ValueError 발생
@@ -16,6 +13,16 @@ while(누적num<31):
             print("정수를 입력하세요.")
 
 
+
+
+#A
+누적num=0
+while(누적num<31):
+    num=0
+    BR31 = [i for i in range(1, 32)]
+    
+
+    num=brGame()
     for i in range(num):
             누적num += 1
             print("PlayerA:", BR31[누적num - 1])
@@ -28,19 +35,8 @@ while(누적num<31):
         break
 
 
-    #B
-    
-    while True:
-        try:
-            num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-            if 1 <= num <= 3:
-                break
-            else:
-                print("1, 2, 3 중 하나를 입력하세요.")
-        #입력받은 값을 int()를 사용하여 정수로 변환을 시도 but 실패시 ValueError 발생
-        except ValueError:
-            print("정수를 입력하세요.")
-
+    #B 
+    num=brGame()
     for i in range(num):
         누적num += 1
         print("PlayerB:", BR31[누적num - 1])
