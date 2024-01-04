65969a946344d667e518b8e7626b6b1bcfa715d8
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2024-01-04 19:50:23 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,34 +1,50 @@
 #A
 num=0
 BR31 = [i for i in range(1, 32)]
-while True:
-    try:
-        num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-        if 1 <= num <= 3:
+누적num=0
+
+while(누적num<31):
+    while True:
+        try:
+            num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+            if 1 <= num <= 3:
+                break
+            else:
+                print("1, 2, 3 중 하나를 입력하세요.")
+        #입력받은 값을 int()를 사용하여 정수로 변환을 시도 but 실패시 ValueError 발생
+        except ValueError:
+            print("정수를 입력하세요.")
+
+
+    for i in range(num):
+            누적num += 1
+            print("PlayerA:", BR31[누적num - 1])
+            if 누적num >= 31:
+                break
+
+    #B로 넘어감 방지
+    if 누적num >= 31:
+        break
+
+
+    #B
+    
+    while True:
+        try:
+            num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+            if 1 <= num <= 3:
+                break
+            else:
+                print("1, 2, 3 중 하나를 입력하세요.")
+        #입력받은 값을 int()를 사용하여 정수로 변환을 시도 but 실패시 ValueError 발생
+        except ValueError:
+            print("정수를 입력하세요.")
+
+    for i in range(num):
+        누적num += 1
+        print("PlayerB:", BR31[누적num - 1])
+
+        if 누적num >= 31:
             break
-        else:
-            print("1, 2, 3 중 하나를 입력하세요.")
-    #입력받은 값을 int()를 사용하여 정수로 변환을 시도 but 실패시 ValueError 발생
-    except ValueError:
-        print("정수를 입력하세요.")
 
 
-for i in range(num):
-    print("playerA:",BR31[i])
-
-numA=num
-#B
-num=0
-while True:
-    try:
-        num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-        if 1 <= num <= 3:
-            break
-        else:
-            print("1, 2, 3 중 하나를 입력하세요.")
-    #입력받은 값을 int()를 사용하여 정수로 변환을 시도 but 실패시 ValueError 발생
-    except ValueError:
-        print("정수를 입력하세요.")
-
-for i in range(num):
-    print("playerB:",BR31[i+numA])
