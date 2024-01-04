4caf596ae6da8f2a428b2b34ba867570cfa57032
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2024-01-04 18:58:26 +0900

    4단계-수정

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,4 +1,5 @@
 num=0
+BR31 = [i for i in range(1, 32)]
 while True:
     try:
         num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
@@ -10,4 +11,6 @@ while True:
     except ValueError:
         print("정수를 입력하세요.")
 
-print("입력받은 숫자:",num)
+
+for i in range(num):
+    print("playerA:",BR31[i])
