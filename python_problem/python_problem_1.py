16b5d61a452701565d34b71cb348a8ce6c89a128
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2024-01-04 18:47:03 +0900

    3단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,2 +1,13 @@
 num=0
-num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+while True:
+    try:
+        num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+        if 1 <= num <= 3:
+            break
+        else:
+            print("1, 2, 3 중 하나를 입력하세요.")
+    #입력받은 값을 int()를 사용하여 정수로 변환을 시도 but 실패시 ValueError 발생
+    except ValueError:
+        print("정수를 입력하세요.")
+
+
